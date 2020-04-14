abbc07d00b877dad553b9c0457e607e8b9c6dfef
Andoni <andonimarzo@gmail.com>
2020-04-14 14:05:07 +0200

    Componentes React Native

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Campobase from './componentes/CampobaseComponent'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Bienvenido a la aplicación Gaztaroa!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Campobase />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#eaa152',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
