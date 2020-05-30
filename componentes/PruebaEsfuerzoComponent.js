d3c64a964d252672db1e62b413095b4a3711985b
Andoni <andonimarzo@gmail.com>
2020-05-30 22:18:07 +0200

    Calendario

--- componentes/PruebaEsfuerzoComponent.js
+++ componentes/PruebaEsfuerzoComponent.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Platform } from 'react-native';
 import DatePicker from 'react-native-datepicker'
 import { colorGaztaroaOscuro } from '../comun/comun';
 import * as Animatable from 'react-native-animatable';
+import * as Calendar from 'expo-calendar';
 
 class PruebaEsfuerzo extends Component {
-
     constructor(props) {
         super(props);
 
@@ -17,9 +17,9 @@ class PruebaEsfuerzo extends Component {
         }
     }
 
-
     gestionarReserva() {
         console.log(JSON.stringify(this.state));
+        this.addCalendario();
         this.toggleModal();
     }
 
@@ -34,6 +34,89 @@ class PruebaEsfuerzo extends Component {
             fecha: '',
             showModal: false
         });
+    }
+
+    // Calendario
+    addCalendario = async () => {
+
+        // Pedimos permiso para usar el calendario
+        const { status } = await Calendar.requestCalendarPermissionsAsync();
+
+        // Una vez que nos ha concedido permiso
+        if (status === 'granted') {
+
+            // Obtenemos todos los calendarios y comprobamos si existe ya uno con nombre Expo Calendar
+            const calendarios = await Calendar.getCalendarsAsync();
+            const calendarioExpo = calendarios.filter(calendario => calendario.source.name === 'Expo Calendar');
+
+            let calendarioExpoID = 0;
+            if (calendarioExpo.length > 0) {
+                // En caso de que exista el calendario, obtenemos la ID
+                calendarioExpoID = calendarioExpo[0].id
+                //console.log(calendarioExpoID)
+                //Calendar.deleteCalendarAsync(calendarioExpoID)
+            } else {
+                // En caso de que no exista, creamos el calendario y obtenemos la ID
+                console.log("No existe ningún Expo Calendar")
+                calendarioExpoID = await this.createCalendar();
+                //console.log(calendarioExpoID)
+            }
+
+            // Añadimos la prueba en el calendario
+            //console.log(calendarioExpoID)
+            await Calendar.createEventAsync(calendarioExpoID, {
+                startDate: new Date(this.state.fecha),
+                endDate: new Date(this.calcularFin()),
+                title: "Prueba de esfuerzo"
+            })
+                .then(event => {
+                    console.log('success', event);
+                })
+                .catch(error => {
+                    console.log('failure', error);
+                });
+        }
+    }
+
+    calcularFin = () => {
+        const a = this.state.fecha
+        const b = a.split("T")
+        const c = b[1].split(":")
+        const d = parseInt(c[0]) + 1
+        let e = null
+        c.forEach((valor, indice) => {
+            if (indice > 0) {
+                e = e + ":" + valor
+            } else {
+                e = d
+            }
+        });
+        return (b[0] + "T" + e)
+    }
+
+    getDefaultCalendarSource = async () => {
+        const calendars = await Calendar.getCalendarsAsync();
+        const defaultCalendars = calendars.filter(each => each.source.name === 'Default');
+        return defaultCalendars[0].source;
+    }
+
+    createCalendar = async () => {
+        const defaultCalendarSource =
+            Platform.OS === 'ios'
+                ? await getDefaultCalendarSource()
+                : { isLocalAccount: true, name: 'Expo Calendar' };
+        const newCalendarID = await Calendar.createCalendarAsync({
+            title: 'Expo Calendar',
+            color: 'blue',
+            entityType: Calendar.EntityTypes.EVENT,
+            sourceId: defaultCalendarSource.id,
+            source: defaultCalendarSource,
+            name: 'internalCalendarName',
+            ownerAccount: 'personal',
+            accessLevel: Calendar.CalendarAccessLevel.OWNER,
+        });
+        console.log(`Your new calendar ID is: ${newCalendarID}`);
+        return newCalendarID
     }
 
     render() {
